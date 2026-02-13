b23453967c73bbebb421448af26d78b60768d2f9
eststar <eststar1@naver.com>
2026-02-13 20:17:11 +0900

    v0.6.7

--- src/app/main/studio/components/UploadPanel.tsx
+++ src/app/main/studio/components/UploadPanel.tsx
@@ -5,7 +5,7 @@ import { FaCloudArrowUp, FaXmark, FaMagnifyingGlass, FaCircleInfo, FaExpand, FaF
 import Image from 'next/image';
 import { RecommendData } from '@/types/ProductType';
 import { getRecommendList } from '@/app/api/productService/productapi';
-import { postImage } from '@/app/api/imageService/Imageapi';
+import { postImage } from '@/app/api/imageservice/imageapi';
 
 interface UploadPanelProps {
   onResultFound: (results: any[] | null) => void;
